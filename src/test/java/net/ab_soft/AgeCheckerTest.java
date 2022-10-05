cf2630af26fb61549db10321515abaa8b2aa5470
Aleksey Loginov <aleksey.loginov@ab-soft.net>
2022-10-05 15:54:54 +0300

    Test User Can't Play

--- src/test/java/net/ab_soft/AgeCheckerTest.java
+++ src/test/java/net/ab_soft/AgeCheckerTest.java
@@ -8,6 +8,13 @@ public class AgeCheckerTest {
     @Test
     public void testAgedUserCanPlay() {
         AgeChecker checkAge = new AgeChecker();
-        Assert.assertTrue(checkAge.canPlayGames(19), "Aged user can play game");
+        Assert.assertTrue(checkAge.canPlayGames(21), "Aged user can play game");
+    }
+
+
+    @Test
+    public void testThatTooYongUsersCanNotPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGames(20), "Aged user can't play game");
     }
 }
